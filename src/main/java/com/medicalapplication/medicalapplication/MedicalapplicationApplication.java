e0b9e99d00120bdeaee8f96a9bd5bdd8ea0b232c
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-08-20 11:47:42 +0530

    database-coonection-created

---
+++ src/main/java/com/medicalapplication/medicalapplication/MedicalapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.medicalapplication.medicalapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicalapplication/medicalapplication/MedicalapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalapplication.medicalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
